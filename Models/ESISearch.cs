6004abe9acae459e59c6711894b300f260104a27
Kristen Tidmuss <11579255+KristenTidmuss@users.noreply.github.com>
2021-02-28 21:49:29 +0000

    Generate Template

--- Models/ESISearch.cs
+++
@@ -1,9 +0,0 @@
-namespace EveConnectionFinder.Models
-{
-    public class ESISearch
-    {
-        public long[] alliance { get; set; }
-        public long[] character { get; set; }
-        public long[] corporation { get; set; }
-    }
-}
